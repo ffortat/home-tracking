2f1ffd812338e0954be11dfc47ea0676fb84541c
Florent Fortat <florent@fortat.fr>
2020-04-13 00:19:42 +0200

    Add base component for new product line

---
+++ src/app/product/add-product/product-line/product-line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-line',
+  templateUrl: './product-line.component.html',
+  styleUrls: ['./product-line.component.scss']
+})
+export class ProductLineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
