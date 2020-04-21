0cad36c648fbd9fd9528c58a0844d258d7ece645
Florent Fortat <florent@fortat.fr>
2020-04-22 00:53:29 +0200

    Add task module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from '../common/components/navigation/navigation.module';
-import { TaskComponent } from './task/task.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductModule } from './product/product.module';
@@ -13,7 +12,6 @@ import { SharedMaterialModule } from '../common/modules/shared-material.module';
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
   ],
   imports: [
     BrowserModule,
