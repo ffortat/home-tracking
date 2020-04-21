165d804e7f16e057366a5a78bed557cd3cba13e5
Florent Fortat <florent@fortat.fr>
2020-04-22 00:51:01 +0200

    Add task service

---
+++ src/app/task/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public getTasks(): Observable<any> {
+    return this.http.get(environment.apiUrl + '/api/task');
+  }
+
+  public addTask(task: any): Observable<any> {
+    return this.http.post(environment.apiUrl + '/api/task', task);
+  }
+}
